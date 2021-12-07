e41bf2b8cab04b0ebbfeb07b7a4a80c51014bb2c
LinoaHeartilly <alexandre.caniac93@gmail.com>
2021-12-07 10:04:15 +0100

    Modification variable

--- variable.py
+++ variable.py
@@ -1,5 +1,21 @@
+# déclaration variable
 livre = "Harry Potter"
+
+# Affichage dans la console
 print(livre)
 
 livre = livre + " 2"
 print(livre)
+
+# liste
+plateformes_sociales = ["Facebook", "Instagram", "Snapchat", "Twitter"]
+print(plateformes_sociales[0])
+# Ajout puis remove dans liste
+plateformes_sociales.append('TikTok')
+print(plateformes_sociales)
+print(plateformes_sociales[4])
+plateformes_sociales.remove(plateformes_sociales[2])
+print(plateformes_sociales)
+taille ='Taille de la liste : ' +str(len(plateformes_sociales))
+print(taille)
+
